a82931109cda2855c15880e56e731dce476849cc
Rogargar <48553336+Rogargar@users.noreply.github.com>
2020-09-09 10:20:56 +0200

    no message

--- finalproject-front/src/app/services/user.service.ts
+++ finalproject-front/src/app/services/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { MapperService } from './mapper.service';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -12,12 +12,20 @@ export class UserService {
 
   private enviroment = environment;
 
+  private headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
   private url = `${this.enviroment.urlBack}/users`;
 
   constructor(private _http: HttpClient, private _mapper: MapperService) { }
 
   getUsers() {
-    return this._http.get(this.url)
+    return this._http.get(this.url, {
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "https://www.example.com",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      }
+    })
       .pipe(
         map((item: any) => {
           item = item.map((user: any) => {
